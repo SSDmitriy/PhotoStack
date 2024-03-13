81b18f72c541208f6d82d8e94a3071a92e9ec06c
Dmitriy Sorokin <d.sorokin@pravo.tech>
2024-03-13 23:52:41 +0400

    Revert "test commit1"
    This reverts commit 627023e5cdc2baf3a8841fc78e21227495402a4d.

--- PhotoStack/PhotoStack.API/Controllers/PhotoCardController.cs
+++ PhotoStack/PhotoStack.API/Controllers/PhotoCardController.cs
@@ -26,6 +26,8 @@ namespace PhotoStack.API.Controllers
         // --написать метод в контроллере на получение всех картинок
         // [HttpGet] по анологии с Create
 
+        // написать метод в контроллере на получение одной карточки по Id
+        // [HttpGet] по анологии с Create
 
         // прочитать про FromRoute, FromBody, FromQuery, FromForm
 
