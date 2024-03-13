52974358bf60abe1c2a86aa5b4829d5b17a68e19
SSDmitriy <xyyzmail@gmail.com>
2024-03-14 02:04:17 +0400

    add fields validation

--- PhotoStack/PhotoStack.Domain/Models/Error.cs
+++ PhotoStack/PhotoStack.Domain/Models/Error.cs
@@ -1,12 +1,28 @@
-﻿namespace PhotoStack.Domain.Models
+﻿
+namespace PhotoStack.Domain.Models
 {
     public record Error(string Message);
 
     public static class GeneralErrors
     {
-        public static Error IsInvalid(string name)
+        public static Error CannotBeEmpty(string name)
         {
-            return new Error($"{name} is invalid");
+            return new Error($"Parameter \'{name}\' can't be empty.");
+        }
+
+        public static Error LongerThan(string name, int maxLength)
+        {
+            return new Error($"Parameter \'{name}\' can't be longer than {maxLength}.");
+        }
+
+        internal static Error LessThan(string name, decimal minValue)
+        {
+            return new Error($"Parameter \'{name}\' can't be less than {minValue}.");
+        }
+
+        internal static Error GreaterThan(string name, decimal maxValue)
+        {
+            return new Error($"Parameter \'{name}\' can't be greater than {maxValue}.");
         }
     }
 }
